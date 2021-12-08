e69a30c2dc1bda4038ce6bad1109fefad6127dcb
Sattawat Tusumran <s6104062616163@email.kmutnb.ac.th>
2021-12-08 13:13:29 +0700

    V2

--- Member.py
+++ Member.py
@@ -1,9 +1,24 @@
 from flask import Blueprint,render_template,request,redirect,url_for,session
 import pymysql
 from config import *
+import os
 
 con = pymysql.connect(HOST,USER,PASS,DATABASE)
 member = Blueprint('member',__name__)
+
+@member.route("/showdatamembersomeone",methods=["POST"])
+def Showsomeone():
+    if "username" not in session:
+        return render_template('login.html',headername="Login เข้าใช้งานระบบ")
+    if request.method == "POST":
+        id = request.form["id"]
+        with  con:
+            cur = con.cursor()
+            sql = "SELECT * FROM tb_member where mem_id = %s"
+            cur.execute(sql,(id))
+            rows = cur.fetchall()
+            return render_template("showdatamebersomeone.html",headername="ข้อมูลสมาชิก",datas=rows)
+
 @member.route("/showmember")
 def Showdatamember():
     if "username" not in session:
@@ -13,7 +28,7 @@ def Showdatamember():
         sql = "SELECT * FROM tb_member"
         cur.execute(sql)
         rows = cur.fetchall()
-        return render_template("Showdatamember.html",headername="ข้อมูลสมาชิก",datas=rows)
+        return render_template("Showdatamember copy.html",headername="ข้อมูลสมาชิก",datas=rows)
 
 @member.route("/showwithdate",methods=["POST"])
 def Showwithdate():
@@ -33,16 +48,34 @@ def Showwithdate():
 @member.route("/editmember",methods=["POST"])
 def Editmember():
     if request.method == "POST":
+
         id = request.form["id"]
         fname = request.form["fname"]
         lname = request.form["lname"]
         email = request.form["email"]
-        with con :
-            cur = con.cursor()
-            sql = "update tb_member set mem_fname = %s,mem_lname = %s,mem_email = %s where mem_id = %s"
-            cur.execute(sql,(fname,lname,email,id))
-            con.commit()
-            return redirect(url_for('member.Showdatamember'))
+        file = request.files['files']
+        if file.filename =="":
+            #update with no pic
+            with con :
+                cur = con.cursor()
+                sql = "update tb_member set mem_fname = %s,mem_lname = %s,mem_email = %s where mem_id = %s"
+                cur.execute(sql,(fname,lname,email,id))
+                con.commit()
+                return redirect(url_for('member.Showdatamember'))
+        else:
+                #update with pic
+                file = request.files['files']
+                upload_folder = 'static/images'
+                app_folder = os.path.dirname(__file__)
+                img_folder = os.path.join(app_folder,upload_folder)
+                file.save(os.path.join(img_folder,file.filename))
+                path = upload_folder + "/" + file.filename
+                with con :
+                    cur = con.cursor()
+                    sql = "update tb_member set mem_fname = %s,mem_lname = %s,mem_email = %s, mem_pic = %s where mem_id = %s"
+                    cur.execute(sql,(fname,lname,email,path,id))
+                    con.commit()
+                    return redirect(url_for('member.Showdatamember'))
 
 @member.route("/delmember",methods=["POST"])
 def Delmember():
@@ -62,12 +95,19 @@ def Adddatamember():
 @member.route("/adddata",methods=["POST"])
 def Adddata():
     if request.method == "POST":
+        file = request.files['files']
+        upload_folder = 'static/images'
+        app_folder = os.path.dirname(__file__)
+        img_folder = os.path.join(app_folder,upload_folder)
+        file.save(os.path.join(img_folder,file.filename))
+        path = upload_folder + "/" + file.filename
+
         fname = request.form["fname"]
         lname = request.form["lname"]
         email = request.form["email"]
         with con :
             cur = con.cursor()
-            sql = "insert into tb_member (mem_fname,mem_lname,mem_email) VALUES (%s,%s,%s)"
-            cur.execute(sql,(fname,lname,email))
+            sql = "insert into tb_member (mem_fname,mem_lname,mem_email,mem_pic) VALUES (%s,%s,%s,%s)"
+            cur.execute(sql,(fname,lname,email,path))
             con.commit()
             return redirect(url_for('member.Showdatamember'))
